1e8ca3eb930afcb725b57bbfbc396bbea5c0af54
Vika <89023983614@mail.ru>
2025-09-24 22:23:34 +0300

    Изменила функцию

--- src/height.cpp
+++ src/height.cpp
@@ -1,9 +1,12 @@
 #include "../include/height.h"
 
 int height(int UpSpeed,int DownSpeed,int desiredHeight){
-    int result = desiredHeight / (UpSpeed - DownSpeed);
-    if (desiredHeight % (UpSpeed - DownSpeed) > 0){
-        result += 1;
+    int resylt;
+    int delta;
+    delta = UpSpeed - DownSpeed;
+    resylt = desiredHeight / (UpSpeed - DownSpeed);
+    if (desiredHeight % delta != 0){
+        resylt = resylt + 1;
     }
-    return result;
+    return resylt;
 }

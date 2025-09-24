593802ca38b23dede817dff5a79592f63423af18
Vika <89023983614@mail.ru>
2025-09-24 22:27:48 +0300

    .

--- src/height.cpp
+++ src/height.cpp
@@ -1,12 +1,9 @@
 #include "../include/height.h"
 
 int height(int UpSpeed,int DownSpeed,int desiredHeight){
-    int resylt;
-    int delta;
-    delta = UpSpeed - DownSpeed;
-    resylt = desiredHeight / (UpSpeed - DownSpeed);
-    if (desiredHeight % delta != 0){
-        resylt = resylt + 1;
+    int result = desiredHeight / (UpSpeed - DownSpeed);
+    if (desiredHeight % (UpSpeed - DownSpeed) > 0){
+        result += 1;
     }
-    return resylt;
+    return result;
 }
